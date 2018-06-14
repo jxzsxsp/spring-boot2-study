86bcb41a1a24cda4b99fe7c09977b3eeda9912e8
Sean.Xiao <jxzsxsp@qq.com>
2018-06-14 19:21:35 +0800

    spring boot2 webflux thymeleaf

---
+++ spring-boot-webflux/src/main/java/cn/xiaoshuiping/springbootwebflux/constant/TemplateConstant.java
@@ -0,0 +1,12 @@
+package cn.xiaoshuiping.springbootwebflux.constant;
+
+/**
+ * @author Sean.Xiao
+ * @date 2018/6/14 下午6:40
+ */
+public class TemplateConstant {
+
+    public static final String HELLO = "hello";
+    public static final String PERSON_LIST = "personList";
+
+}
--- spring-boot-webflux/src/main/java/cn/xiaoshuiping/springbootwebflux/controller/PersonController.java
+++ spring-boot-webflux/src/main/java/cn/xiaoshuiping/springbootwebflux/controller/PersonController.java
@@ -1,8 +1,11 @@
 package cn.xiaoshuiping.springbootwebflux.controller;
 
+import cn.xiaoshuiping.springbootwebflux.constant.TemplateConstant;
 import cn.xiaoshuiping.springbootwebflux.domain.Person;
 import cn.xiaoshuiping.springbootwebflux.handler.PersonHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,7 +14,7 @@ import reactor.core.publisher.Mono;
  * @author Sean.Xiao
  * @date 2018/6/12 下午7:55
  */
-@RestController
+@Controller
 @RequestMapping(value = "/person")
 public class PersonController {
 
@@ -19,27 +22,42 @@ public class PersonController {
     private PersonHandler personHandler;
 
     @GetMapping(value = "/{id}")
-    public Mono<Person> findPersonById(@PathVariable(value = "id") Long id) {
+    public @ResponseBody Mono<Person> findPersonById(@PathVariable(value = "id") Long id) {
         return personHandler.findPersonById(id);
     }
 
     @GetMapping()
-    public Flux<Person> findAllPerson() {
+    public @ResponseBody Flux<Person> findAllPerson() {
         return personHandler.findAllPerson();
     }
 
     @PostMapping()
-    public Mono<Person> savePerson(@RequestBody Person person) {
+    public @ResponseBody Mono<Person> savePerson(@RequestBody Person person) {
         return personHandler.save(person);
     }
 
     @PutMapping()
-    public Mono<Person> modifyPerson(@RequestBody Person person) {
+    public @ResponseBody Mono<Person> modifyPerson(@RequestBody Person person) {
         return personHandler.modifyPerson(person);
     }
 
     @DeleteMapping(value = "/{id}")
-    public Mono<Long> deletePerson(@PathVariable(value = "id") Long id) {
+    public @ResponseBody Mono<Long> deletePerson(@PathVariable(value = "id") Long id) {
         return personHandler.deletePerson(id);
+    }
+
+    @GetMapping(value = "/hello")
+    public Mono<String> helloThymeleaf(final Model model) {
+        model.addAttribute("title", "你好，Thymeleaf!");
+        model.addAttribute("desc", "这是我的第一个Thymeleaf模板页面。");
+
+        return Mono.create(monoSink -> monoSink.success(TemplateConstant.HELLO));
+    }
+
+    @GetMapping(value = "/page/list")
+    public String personList(final Model model) {
+        final Flux<Person> personList = personHandler.findAllPerson();
+        model.addAttribute("personList", personList);
+        return TemplateConstant.PERSON_LIST;
     }
 }
